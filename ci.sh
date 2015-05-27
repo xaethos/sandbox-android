c4cd55aa9c67b084428bce94158bf115fd505d1d
Diego García <xaethos@gmail.com>
2015-05-28 01:08:18 +0800

    set build type

--- ci.sh
+++ ci.sh
@@ -3,14 +3,14 @@ then
   echo "This is Jenkins build $BUILD_NUMBER!"
   echo "Branch: $GIT_BRANCH"
   IFS=/ read -r GIT_REMOTE BUILD_TYPE BUILD_TOPIC <<<"$GIT_BRANCH"
+  curl -s --data-urlencode "description=$BUILD_TYPE $BUILD_TOPIC" "${BUILD_URL}submitDescription"
 else
   echo "No Jenkins here. Move along..."
   BUILD_TYPE=develop
   WORKSPACE=`git rev-parse --show-toplevel`
 fi
 
-echo "Type: $BUILD_TYPE"
-test -n "$BUILD_TOPIC" && echo "Topic: $BUILD_TOPIC"
+echo "Build type: $BUILD_TYPE"
 
 cd $WORKSPACE
 ./gradlew assembleDebug
