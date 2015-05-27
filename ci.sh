27240f8e61cb985bb0bf3f7486e42e91eb986e58
Diego García <xaethos@gmail.com>
2015-05-27 23:02:56 +0800

    add branch type

--- ci.sh
+++ ci.sh
@@ -1,9 +1,9 @@
 if [ -n "$JENKINS_HOME" ]
 then
-  echo "This seems to be a jenkins build!"
-
-  echo "BUILD_NUMBER=$BUILD_NUMBER"
-  echo "GIT_BRANCH=$GIT_BRANCH"
+  echo "This Jenkins build $BUILD_NUMBER!"
+  echo "Branch: $GIT_BRANCH"
+  BRANCH_TYPE=`echo $GIT_BRANCH | sed -e 's#[^/]*/\([^/]*\).*#\1#'`
+  echo "Type: $BRANCH_TYPE"
 else
   echo "No Jenkins here. Move along..."
 fi
