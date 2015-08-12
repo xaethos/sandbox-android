9ced980280be03547558890d8cc71d1e4c1d8507
Diego García <xaethos@gmail.com>
2015-08-13 05:34:07 +0800

    Enable input errors

--- app/src/main/java/net/xaethos/sandbox/fragments/PrettyFormFragment.java
+++ app/src/main/java/net/xaethos/sandbox/fragments/PrettyFormFragment.java
@@ -1,6 +1,7 @@
 package net.xaethos.sandbox.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +15,18 @@ public class PrettyFormFragment extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_pretty_form, container, false);
+
+        setUpTextInputLayout(root, R.id.input_thingamajig);
+        setUpTextInputLayout(root, R.id.input_email);
+        setUpTextInputLayout(root, R.id.input_fiddlesticks);
+
         return root;
+    }
+
+    private TextInputLayout setUpTextInputLayout(View container, int viewId) {
+        TextInputLayout input = (TextInputLayout) container.findViewById(viewId);
+        input.setErrorEnabled(true);
+        return input;
     }
 
 }
