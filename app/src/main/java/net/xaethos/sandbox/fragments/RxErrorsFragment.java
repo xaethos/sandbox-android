5252c1299d06e12545437221dc7c5cbc8b896452
Diego García <xaethos@gmail.com>
2015-08-26 09:25:45 +0800

    Song request card

--- app/src/main/java/net/xaethos/sandbox/fragments/RxErrorsFragment.java
+++ app/src/main/java/net/xaethos/sandbox/fragments/RxErrorsFragment.java
@@ -20,6 +20,9 @@ import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
 public class RxErrorsFragment extends Fragment {
+    private static final int[] SONGS =
+            {R.array.bottles_of_beer, R.array.little_ducks, R.array.monkeys_on_the_bed
+            };
 
     private final Random mRand;
 
@@ -36,6 +39,7 @@ public class RxErrorsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_rx_errors, container, false);
 
         setUpCountCard(root.findViewById(R.id.count), mSubscriptions);
+        setUpSongCard(root.findViewById(R.id.song), mSubscriptions);
 
         return root;
     }
@@ -50,28 +54,71 @@ public class RxErrorsFragment extends Fragment {
     private void setUpCountCard(View card, CompositeSubscription subscriptions) {
         TextView textView = (TextView) card.findViewById(R.id.text);
 
-        Observable<CharSequence> observable =
-                Observable.create(new Observable.OnSubscribe<Integer>() {
-                    @Override
-                    public void call(Subscriber<? super Integer> subscriber) {
-                        while (!subscriber.isUnsubscribed()) {
-                            subscriber.onNext(rand(5, 99));
-                            try {
-                                Thread.sleep(rand(3000, 10000));
-                            } catch (InterruptedException e) {
-                                if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
-                                return;
-                            }
-                        }
-                    }
-                }).map(new Func1<Integer, CharSequence>() {
-                    @Override
-                    public CharSequence call(Integer integer) {
-                        return String.format("%d!", integer);
-                    }
-                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        subscriptions.add(countObservable().map(new Func1<Integer, CharSequence>() {
+            @Override
+            public CharSequence call(Integer integer) {
+                return String.format("%d!", integer);
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new TextViewSetTextAction(textView)));
+    }
 
-        subscriptions.add(observable.subscribe(new TextViewSetTextAction(textView)));
+    private void setUpSongCard(View card, CompositeSubscription subscriptions) {
+        TextView textView = (TextView) card.findViewById(R.id.text);
+
+        subscriptions.add(songObservable().map(new Func1<Integer, CharSequence>() {
+            @Override
+            public CharSequence call(Integer songRes) {
+                switch (songRes) {
+                case R.array.monkeys_on_the_bed:
+                    return "Monkeys!";
+                case R.array.little_ducks:
+                    return "Ducks!";
+                case R.array.bottles_of_beer:
+                    return "Beer!";
+                }
+                return "Uhh...";
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new TextViewSetTextAction(textView)));
+    }
+
+    private Observable<Integer> countObservable() {
+        return Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                while (!subscriber.isUnsubscribed()) {
+                    subscriber.onNext(rand(5, 99));
+                    try {
+                        Thread.sleep(rand(3000, 10000));
+                    } catch (InterruptedException e) {
+                        if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
+                        return;
+                    }
+                }
+            }
+        });
+    }
+
+    private Observable<Integer> songObservable() {
+        return Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                while (!subscriber.isUnsubscribed()) {
+                    subscriber.onNext(SONGS[mRand.nextInt(SONGS.length)]);
+                    try {
+                        Thread.sleep(rand(8000, 16000));
+                    } catch (InterruptedException e) {
+                        if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
+                        return;
+                    }
+                }
+            }
+        }).distinctUntilChanged();
     }
 
     private int rand(int lower, int upper) {
