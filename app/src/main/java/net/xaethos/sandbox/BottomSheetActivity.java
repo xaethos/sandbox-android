6428776e0125bac82ed17d88d3d57243cd3e8e43
Diego García <xaethos@gmail.com>
2015-09-09 09:58:30 +0800

    Use Flipboard sheet...

--- app/src/main/java/net/xaethos/sandbox/BottomSheetActivity.java
+++ app/src/main/java/net/xaethos/sandbox/BottomSheetActivity.java
@@ -2,43 +2,64 @@ package net.xaethos.sandbox;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 
-import net.xaethos.sandbox.views.BottomSheetView;
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.OnSheetDismissedListener;
 
 public class BottomSheetActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_sheet);
 
-        final BottomSheetView sheetView = (BottomSheetView) findViewById(R.id.modal_background);
-        sheetView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        setContentView(R.layout.activity_bottom_sheet_2);
+
+//        BottomSheetLayout bottomSheetLayout = new BottomSheetLayout(this);
+        final BottomSheetLayout bottomSheetLayout =
+                (BottomSheetLayout) findViewById(R.id.bottomsheet);
+
+//        setContentView(bottomSheetLayout);
+        bottomSheetLayout.showWithSheetView(LayoutInflater.from(this)
+                        .inflate(R.layout.bottom_sheet, bottomSheetLayout, false),
+                null,
+                new OnSheetDismissedListener() {
+
+                    @Override
+                    public void onDismissed(BottomSheetLayout bottomSheetLayout) {
+                        finish();
+                    }
+                });
+
+//        setContentView(R.layout.activity_bottom_sheet);
+
+//        final BottomSheetView sheetView = (BottomSheetView) findViewById(R.id.modal_background);
+//        sheetView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
 
         findViewById(R.id.btn_fill).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sheetView.fill();
+                bottomSheetLayout.expandSheet();
             }
         });
 
         findViewById(R.id.btn_wrap).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sheetView.wrap();
+                bottomSheetLayout.peekSheet();
             }
         });
 
         findViewById(R.id.btn_dismiss).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sheetView.hide();
+                bottomSheetLayout.dismissSheet();
             }
         });
     }
